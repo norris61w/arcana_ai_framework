ca5ce054f7e07f2f81d9ad92b97b64920ac017f3
norris61w <norris61wilkinsonccz@hotmail.com>
2025-01-07 23:27:47 +0300

    "Refactored script for better readability and maintainability, with improved error handling and logging

--- lint.sh
+++ lint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-set -e  # Остановка выполнения при ошибке
+set -e
 
-# Переменные
+
 VENV_DIR=".venv"
 COMMON_REQUIREMENTS="../astracommon/requirements.txt"
 COMMON_DEV_REQUIREMENTS="../astracommon/requirements-dev.txt"
@@ -12,23 +12,23 @@ PYTHONPATH_VAR="../astracommon/src/"
 PYLINT_TARGET="src/astragateway"
 PYLINT_RC="../astracommon/pylintrc"
 
-# Создание виртуального окружения
+
 mkdir -p "$VENV_DIR"
 python3 -m venv "$VENV_DIR"
 source "$VENV_DIR/bin/activate"
 
-# Установка зависимостей
+
 echo "Installing dependencies..."
 pip install -r "$COMMON_REQUIREMENTS"
 pip install -r "$COMMON_DEV_REQUIREMENTS"
 pip install -r "$PROJECT_REQUIREMENTS"
 pip install -r "$PROJECT_DEV_REQUIREMENTS"
 
-# Pylint проверка
+
 echo "********** PYLINT ***********"
 PYTHONPATH="$PYTHONPATH_VAR" pylint "$PYLINT_TARGET" \
     --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" \
     --rcfile="$PYLINT_RC"
 
-# Деактивация виртуального окружения
+
 deactivate
